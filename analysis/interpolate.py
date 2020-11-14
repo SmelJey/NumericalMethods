7040468c1e03f4dc93e10a7934f69518df7b7569
SmelJey <mixalevuriy@gmail.com>
2020-11-14 19:50:51 +1000

    Add interpolation formulas

---
+++ analysis/interpolate.py
@@ -0,0 +1,88 @@
+def lagrange(x, xArr, yArr):
+    res = 0
+    for i in range(len(xArr)):
+        mult = 1
+        for j in range(len(xArr)):
+            if i != j:
+                mult *= (x - xArr[j]) / (xArr[i] - xArr[j])
+        res += yArr[i] * mult
+    return res
+
+
+def omegaN(x, xArr):
+    res = 1
+    for i in range(0, len(xArr)):
+        res *= x - xArr[i]
+    return res
+
+
+def __calculateDiff(diffMatrix, yArr):
+    curDiffs = []
+    for i in range(1, len(yArr)):
+        curDiffs.append(yArr[i] - yArr[i - 1])
+    diffMatrix.append(curDiffs)
+    if len(curDiffs) > 1:
+        __calculateDiff(diffMatrix, curDiffs)
+
+
+def dividedDiffs(yArr):
+    diffMatrix = [yArr]
+    __calculateDiff(diffMatrix, yArr)
+    return diffMatrix
+
+
+def newton1(xStar, h, xArr, diffMatrix):
+    res = 0
+    mult = 1
+    t = (xStar - xArr[0]) / h
+    if not (0 < t < 1):
+        raise Exception('xStar must be in the start of the table')
+
+    for i in range(0, len(diffMatrix)):
+        res += diffMatrix[i][0] * mult
+        mult *= (t - i) / (i + 1)
+
+    return res
+
+
+def newton2(xStar, h, xArr, diffMatrix):
+    res = 0
+    mult = 1
+    t = (xStar - xArr[-1]) / h
+    if not -1 < t < 0:
+        raise Exception('xStar must be in the end of the table')
+
+    for i in range(0, len(diffMatrix)):
+        res += diffMatrix[i][-1] * mult
+        mult *= (t + i) / (i + 1)
+
+    return res
+
+
+def getClosestIdx(xArr, x):
+    for i in range(0, len(xArr)):
+        if x < xArr[i]:
+            return i - 1
+    return -1
+
+
+def gauss12(xStar, h, xArr, diffMatrix):
+    closestIdx = getClosestIdx(xArr, xStar)
+    t = (xStar - xArr[closestIdx]) / h
+    if not (-0.5 < t < 0.5):
+        raise Exception('incorrect xStar')
+    mult = 1
+    res = 0
+    if t > 0:
+        for i in range(0, len(diffMatrix)):
+            res += diffMatrix[i][closestIdx] * mult
+            mult *= (t + ((-1) ** i) * ((i + 1) // 2)) / (i + 1)
+            if i % 2 == 1:
+                closestIdx -= 1
+    else:
+        for i in range(0, len(diffMatrix)):
+            res += diffMatrix[i][closestIdx] * mult
+            mult *= (t + ((-1) ** i) * (i + 1) // 2) / (i + 1)
+            if i % 2 == 1:
+                closestIdx -= 1
+    return res
---
+++ tests/analysis/interpolate.py
@@ -0,0 +1,123 @@
+import unittest
+import math
+import util
+import analysis.interpolate as interpol
+
+
+def f(x):
+    return x * x + math.log(x) - 4
+    # return x**3 - math.sin(x)
+
+
+def secondDer(x):
+    return 2 - 1 / (x ** 2)
+    # return 6 * x + math.sin(x)
+
+
+def thirdDer(x):
+    return 2.0 / (x ** 3)
+    # return math.cos(x) + 6
+
+
+def elevenDer(x):
+    return 3628800 / (x**11)
+
+
+def fact(n):
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
+class InterpolationTest(unittest.TestCase):
+    def prepare_test(self, a, b, xStar):
+        xArr, yArr = util.createPlot(f, a, b, 10)
+        idx = interpol.getClosestIdx(xArr, xStar)
+        return idx, xArr, yArr
+
+    def test_lagrange1(self):
+        a = 1.45
+        b = 1.95
+        xStar = 1.52
+        idx, xArr, yArr = self.prepare_test(a, b, xStar)
+
+        lg1 = interpol.lagrange(xStar, [xArr[idx], xArr[idx + 1]], [yArr[idx], yArr[idx + 1]])
+        minR1 = secondDer(xArr[idx]) * interpol.omegaN(xStar, [xArr[idx], xArr[idx + 1]]) / 2
+        maxR1 = secondDer(xArr[idx + 1]) * interpol.omegaN(xStar, [xArr[idx], xArr[idx + 1]]) / 2
+        r1 = abs(lg1 - f(xStar))
+
+        self.assertGreaterEqual(r1, abs(minR1))
+        self.assertGreaterEqual(abs(maxR1), r1)
+
+    def test_lagrange2(self):
+        a = 1.45
+        b = 1.95
+        xStar = 1.52
+
+        idx, xArr, yArr = self.prepare_test(a, b, xStar)
+
+        localX = [xArr[idx + i] for i in range(-1, 2)]
+        localY = [yArr[idx + i] for i in range(-1, 2)]
+
+        lg2 = interpol.lagrange(xStar, localX, localY)
+        minR2 = thirdDer(xArr[idx + 1]) * interpol.omegaN(xStar, localX) / 6
+        maxR2 = thirdDer(xArr[idx - 1]) * interpol.omegaN(xStar, localX) / 6
+        r2 = abs(lg2 - f(xStar))
+        self.assertGreaterEqual(r2, abs(minR2))
+        self.assertGreaterEqual(abs(maxR2), r2)
+
+    def test_newton1(self):
+        a = 1.5
+        b = 2
+        xStar = 1.52
+
+        idx, xArr, yArr = self.prepare_test(a, b, xStar)
+        h = (b - a) / 10
+
+        diffMatrix = interpol.dividedDiffs(yArr)
+
+        nwt1 = interpol.newton1(xStar, h, xArr, diffMatrix)
+        nwt1Rmin = elevenDer(xArr[-1]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        nwt1Rmax = elevenDer(xArr[0]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        nwt1R = abs(f(xStar) - nwt1)
+        self.assertGreaterEqual(nwt1R, abs(nwt1Rmin))
+        self.assertGreaterEqual(abs(nwt1Rmax), nwt1R)
+
+    def test_newton2(self):
+        a = 1.5
+        b = 2
+        xStar = 1.97
+
+        idx, xArr, yArr = self.prepare_test(a, b, xStar)
+        h = (b - a) / 10
+
+        diffMatrix = interpol.dividedDiffs(yArr)
+
+        nwt2 = interpol.newton2(xStar, h, xArr, diffMatrix)
+        nwt2Rmin = elevenDer(xArr[-1]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        nwt2Rmax = elevenDer(xArr[0]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        nwt2R = abs(f(xStar) - nwt2)
+        self.assertGreaterEqual(nwt2R, abs(nwt2Rmin))
+        self.assertGreaterEqual(abs(nwt2Rmax), nwt2R)
+
+    def test_gauss1(self):
+        a = 1.5
+        b = 2
+        xStar = 1.77
+
+        idx, xArr, yArr = self.prepare_test(a, b, xStar)
+        h = (b - a) / 10
+
+        diffMatrix = interpol.dividedDiffs(yArr)
+
+        gss1 = interpol.gauss12(xStar, h, xArr, diffMatrix)
+        gss1Rmin = elevenDer(xArr[-1]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        gss1Rmax = elevenDer(xArr[0]) * interpol.omegaN(xStar, xArr) / fact(len(xArr))
+        gss1R = abs(f(xStar) - gss1)
+        self.assertGreaterEqual(gss1R, abs(gss1Rmin))
+        self.assertGreaterEqual(abs(gss1Rmax), gss1R)
+
+
+if __name__ == '__main__':
+    unittest.main()
